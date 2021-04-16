f300bf93f4b7c143ff92aa20d4d4d647d3a5d4de
Lucas da Luz <lucasdaluz0399@gmail.com>
2021-04-15 22:22:51 -0300

    All api getters functions from users table

--- Api/Controllers/UsersController.cs
+++ Api/Controllers/UsersController.cs
@@ -1,12 +1,17 @@
-﻿using DataAccess;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using DataAccess;
 using DataServices;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 
 namespace Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("Api/[controller]")]
     public class UsersController : ControllerBase
     {
         private readonly UsersServices _usersServices;
@@ -17,10 +22,125 @@ namespace Api.Controllers
         }
 
         [HttpGet]
-        [Route("Get")]
+        [ProducesResponseType(typeof(List<Users>), 200)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public IActionResult Get()
         {
-            return Ok(_usersServices.GetAll());
+            try
+            {
+                List<Users> listResult = _usersServices.GetAll().ToList();
+
+                if(listResult.Count() > 0)
+                    return Ok(listResult);
+                return NotFound();
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+        
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(Users), 200)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                Users user = _usersServices.GetById(id);
+
+                if(user != null)
+                    return Ok(user);
+                    return NotFound();
+                }
+            catch(Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("Name/{name}")]
+        [ProducesResponseType(typeof(List<Users>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetByName(string name)
+        {
+            try
+            {
+                List<Users> listResult = _usersServices.GetByName(name).ToList();
+
+                if(listResult.Count() > 0)
+                    return Ok(listResult);
+                return NotFound();
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("BetweenDates/{FirstDate}/{LastDate}")]
+        [ProducesResponseType(typeof(List<Users>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetBetweenBirthDates(string startDate, string endDate)
+        {
+            if(DateTime.TryParse(startDate, out DateTime newStartDate) && DateTime.TryParse(endDate, out DateTime newEndDate))
+            {
+                try
+                {
+                    List<Users> listResult = _usersServices.GetBetweenBirthDates(newStartDate, newEndDate).ToList();
+
+                    if(listResult.Count() > 0)
+                        return Ok(listResult);
+                    return NotFound();
+                }
+                catch(Exception e)
+                {
+                    return StatusCode(500, e.Message);
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet]
+        [Route("CPF/{cpf}")]
+        [ProducesResponseType(typeof(Users), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetBetweenBirthDates(string cpf)
+        {
+            Regex regex = new Regex(@"[\D]");
+            cpf = regex.Replace(cpf, "");
+            if(cpf.Length == 11)
+            {
+                try
+                {
+                    Users result = _usersServices.GetByCPF(cpf);
+
+                    if(result != null)
+                        return Ok(result);
+                    return NotFound();
+                }
+                catch(Exception e)
+                {
+                    return StatusCode(500, e.Message);
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
     }
 }
+                
--- DataServices/UsersService.cs
+++ DataServices/UsersService.cs
@@ -21,17 +21,17 @@ namespace DataServices
             return _context.Users.ToList();
         }
 
-        public Users GetById(int id)
+        public Users GetById(int Id)
         {
-            return _context.Users.Find(id);
+            return _context.Users.Find(Id);
         }
 
         public IEnumerable<Users> GetByName(string name)
         {
-            return _context.Users.Where(u => u.Name == name).ToList();
+            return _context.Users.Where(u => u.Name.Contains(name)).ToList();
         }
 
-        public IEnumerable<Users> GetBetweenDates(DateTime startDate, DateTime endDate)
+        public IEnumerable<Users> GetBetweenBirthDates(DateTime startDate, DateTime endDate)
         {
             return _context.Users
                    .Where(u => u.BirthDate >= startDate && u.BirthDate <= endDate).ToList();
@@ -39,12 +39,7 @@ namespace DataServices
 
         public Users GetByCPF(string cpf)
         {
-            cpf = cpf.Replace(@"/\D/g", "");
-            
-            if(cpf.Length == 11)
-                return _context.Users.Where(u => u.CPF == cpf).FirstOrDefault();
-
-            return null;
+            return _context.Users.Where(u => u.CPF == cpf).FirstOrDefault();
         }
     }
 }
